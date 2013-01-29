438d320519068c0c55bd98948b6cbca33d761af8
dduvnjak <dario.duvnjak@gmail.com>
2013-01-29 18:24:16 +0000

    Added standalone Java installation script

---
+++ java/java.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+. ${DIR}/../etc/include.sh
+
+#install java
+case ${OS_TYPE} in
+		"apt")
+			sudo apt-get install openjdk-6-jdk -y
+			;;
+		"yum")
+			yum install java-1.6.0-openjdk -y
+			;;
+esac
